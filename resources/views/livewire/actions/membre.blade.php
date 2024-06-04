b57f75dfe2490b7ab1fe68eaca66d1886dd454bf
plevens <zafitsaraplevenski@gmail.com>
2024-06-04 16:16:06 +0300

    my last change

--- resources/views/livewire/actions/membre.blade.php
+++ resources/views/livewire/actions/membre.blade.php
@@ -43,7 +43,7 @@ new class extends Component
             @if($key->auth_id == Auth::user()->id)
             @foreach($user as $users)
             @foreach($membres as $_membre)
-            @if($_membre->auth_id == Auth::user()->id && !empty($_membre->user_id) && $key->id == $_membre->team_id)
+            @if($_membre->auth_id == Auth::user()->id && $key->id == $_membre->team_id)
             @php
             $date = strtotime($_membre->updated_at);
             $format = date('d/m/Y',$date);
@@ -105,5 +105,83 @@ new class extends Component
             @endif
             @endforeach
         </table>
+        @php
+        $i = 0;
+        @endphp
+
+        @foreach($team_invites as $originTeam)
+        @foreach($membres as $member)
+        @if($member->user_id == Auth::user()->id && $member->activate == "true" && $member->team_id == $originTeam->id)
+        @php
+        $i++;
+        @endphp
+        @endif
+        @endforeach
+        @endforeach
+
+        @if($i == 1)
+
+        <table cellpadding="10%" style="text-align:center">
+            <tr>
+                <td>
+
+                </td>
+                <td>
+                    Pseudo
+                </td>
+                <td>
+                    Email
+                </td>
+                <td>
+                    Role
+                </td>
+                <td>
+                    date d'entrer
+                </td>
+                <td>
+                    Menu
+                </td>
+            </tr>
+            @foreach($membres as $members)
+            @foreach($team as $key)
+            @php
+            $date = strtotime($members->updated_at);
+            $format = date('d/m/Y',$date);
+            @endphp
+            <tr>
+                <td>
+
+                    @if(strlen($members->avatar) == 1)
+                    <b style="background-color:cadetblue;border-radius:2em;padding:20px 20px ;font-weight:bold;">{{$members->avatar}}</b>
+                    @else
+                    <input type="image" src="{{asset('storage/'.$members->avatar)}}" style="border-radius:4em" alt="" width="50cm" height="50cm">
+                    @endif
+                </td>
+                <td>
+                    {{$members->pseudo}}
+                </td>
+                <td>
+                    {{$members->email}}
+                </td>
+                <td>
+                    {{$members->role}}
+                </td>
+                <td>
+                    {{$format}}
+                </td>
+                @if($members->user_id == Auth::user()->id)
+                <td>
+                    (Vous)
+                </td>
+                @else
+                <td>
+                    Apropos
+                </td>
+            </tr>
+            @endif
+            @endforeach
+            @endforeach
+        </table>
+        @endif
     </center>
 </div>
