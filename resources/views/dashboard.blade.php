02de2235b62fdd85728fac74acc7194015b1e8b0
plevens <zafitsaraplevenski@gmail.com>
2024-06-09 10:15:59 +0300

    ple back

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <x-slot name="header" >
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight"id="titre">
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight" id="titre">
             {{ __('Accueil') }}
         </h2>
     </x-slot>
@@ -8,9 +8,9 @@
     <div class="py-12 ">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900" >
+                <div class="p-6 text-gray-900">
                     <center>
-                        ATAOVY SARY ETO DE TEXTE KELY (TSY AIKO)
+                        Vous avez reussit :)
                     </center>
                 </div>
             </div>
